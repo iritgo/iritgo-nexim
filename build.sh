859c9a16457b780011a120257b6958b4bf552d35
Dirk Grappendorf <d.grappendorf@web.de>
2012-10-24 15:47:53 +0200

    Option for running Maven in batch mode

--- build.sh
+++ build.sh
@@ -8,9 +8,10 @@ optFormat=
 optSite=
 optSettings=
 optMavenSettings=
+optBatchMode=
 
 shortOptions=c1h
-longOptions=format,site,settings:
+longOptions=format,site,settings:,batchmode
 
 options=("$(getopt -u -o $shortOptions --long $longOptions -- $@)")
 
@@ -27,6 +28,8 @@ function processOptions
 				optSettings="--settings $1"
 				optMavenSettings="-s $HOME/.m2/$1-settings.xml"
 				;;
+			--batchmode)
+			        optBatchMode=1;;
 			-h) printf "Usage: %s: [options]\n" $0
 			    printf "Options:\n"
 			    printf " -c                              Perform a clean build\n"
@@ -35,6 +38,7 @@ function processOptions
 			    printf " --site                          Update the project web site\n"
 			    printf " --settings                      Alternate Maven settings file\n"
 			    printf "                                 (Leave out the path and the extension)\n"
+			    printf " --batchmode                     Run maven in batch mode\n"
 			    printf " -h                              Print this help\n"
 			    exit 0
 			    ;;
@@ -45,18 +49,24 @@ function processOptions
 
 processOptions $options
 
+MVN="mvn"
 if [ ! -z "$optMavenSettings" ]
 then
-	MVN="mvn $optMavenSettings"
-else
-	MVN="mvn"
+	MVN="$MVN $optMavenSettings"
+fi
+if [ ! -z "$optBatchMode" ]
+then
+	MVN="$MVN -B"
 fi
 
+BUILD="./build.sh"
 if [ ! -z "$optSettings" ]
 then
-	BUILD="./build.sh $optSettings"
-else
-	BUILD="./build.sh"
+	BUILD="$BUILD $optSettings"
+fi
+if [ ! -z "$optBatchMode" ]
+then
+	BUILD="$BUILD $optBatchMode"
 fi
 
 shift $(($OPTIND - 1))
